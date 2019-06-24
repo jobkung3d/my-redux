a7e1c1ec34dfcee4f0f57d3d7daf0679cbb947c3
jobkung3d <crystan_job.a@hotmail.com>
2019-06-24 21:32:25 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { increment, decrement } from './actions/action'
+
+
+/*const App = ({ message, counter, dispatch }) => (
+  <div>
+  <div className="buttons">
+    <button onClick={() => dispatch(increment(1))} className="button is-primary">
+      +1
+    </button>
+    <button onClick={() => dispatch(increment(2))} className="button is-link">
+      +2
+    </button>
+    <button onClick={() => dispatch(increment(3))} className="button is-info">
+      +3
+    </button>
+  </div>
+
+  <div className="buttons">
+    <button onClick={() => dispatch(decrement(1))} className="button is-primary">
+      -1
+    </button>
+    <button onClick={() => dispatch(decrement(2))} className="button is-link">
+      -2
+    </button>
+    <button onClick={() => dispatch(decrement(3))} className="button is-info">
+      -3
+    </button>
+  </div>
+  </div>
+)*/
+
 class App extends Component {
-  render(message, counter, dispatch){
-    return (
+
+  render(){
+    const { dispatch } = this.props;
+    return ( 
       <div className="container">
         <div className="columns column is-12">
-          <h1>Counter : {counter}</h1>
+          <h1>Counter : {this.props.counter}</h1>
         </div>
 
         <div className="buttons">
